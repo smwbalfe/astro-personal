3a2fba594bc5f56a850f2d146f3ed9683eeb5a5d
smwbalfe <smwbalfe@gmail.com>
2025-06-28 00:33:14 +0100

    add partytwon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import icon from "astro-icon";
-import react from '@astrojs/react';
+import react from '@astrojs/react'
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [icon(), react()]
+  integrations: [icon(), react(), partytown(
+    {
+      config: {
+        forward: ['dataLayer.push']
+      }
+    }
+  )]
 });
